25e88a30a871955961dceab394d07993c3e07be6
Cade Santha <cadesantha@gmail.com>
2021-03-15 15:42:35 -0400

    Implemented Payment Section

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Route, BrowserRouter as Router, Switch, Link } from 'react-router-dom';
-import Product from './Products/Products'
-import Home from './Home/Home'
-import Orders from './Orders/Orders'
-import OrderDetail from './Orders/OrderDetails'
+import Product from './Products/Products';
+import Home from './Home/Home';
+import Orders from './Orders/Orders';
+import OrderDetail from './Orders/OrderDetails';
+import Payment from './Payment/Payment';
 
 function App() {
   const openMenu = () => {
@@ -28,6 +29,7 @@ function App() {
                       <Link to="/">Home</Link>
                       <Link to="/catalog">Catalog</Link>
                       <Link to="/orders">Orders</Link>
+                      <Link to="/payment">Payment</Link>
                   </div>
               </header>
               <aside className ="sidebar">
@@ -55,6 +57,9 @@ function App() {
                   </Route>
                   <Route path="/order/:id">
                     <OrderDetail />
+                  </Route>
+                  <Route path="/payment">
+                    <Payment />
                   </Route>
                 </Switch>
               </main>
